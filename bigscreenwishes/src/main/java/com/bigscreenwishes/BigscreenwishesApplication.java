ce1c08fdb46598337e56e4b4b2349039a7ce4302
Shubham Patel <shubhampatel872005@gmail.com>
2025-07-09 02:16:54 +0530

    initital commit

---
+++ bigscreenwishes/src/main/java/com/bigscreenwishes/BigscreenwishesApplication.java
@@ -0,0 +1,13 @@
+package com.bigscreenwishes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigscreenwishesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigscreenwishesApplication.class, args);
+	}
+
+}
